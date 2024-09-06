d824d577aa2ff7bb858408ad963dca8e690ae3d4
lamarwelcome <ultimointento243@gmail.com>
2024-09-05 20:49:00 -0600

    Add files via upload

---
+++ porta.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+// Aquí puedes agregar código JavaScript si es necesario, por ejemplo, para validar formularios
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío del formulario para propósitos de demostración
+    alert('Formulario enviado correctamente');
+});
